f3064e22bfd3770e4856a683d4e074a4a7a69c1e
Aaron Kling <webgeek1234@gmail.com>
2026-04-30 11:42:41 -0500

    drm/panel: chipwealth-ch13726a: Further cleanups for upstreaming
    Change-Id: Ied380bacdba33b78a79e45e41e60bc3f4eedb675

--- drivers/gpu/drm/panel/panel-chipwealth-ch13726a.c
+++ drivers/gpu/drm/panel/panel-chipwealth-ch13726a.c
@@ -17,14 +17,29 @@
 
 #include <video/mipi_display.h>
 
+static const struct regulator_bulk_data ch13726a_supplies[] = {
+	{ .supply = "vdd1v2", },
+	{ .supply = "vddio", },
+	{ .supply = "vdd", },
+	{ .supply = "avdd", },
+};
+
 struct ch13726a_panel {
 	struct drm_panel panel;
 	struct mipi_dsi_device *dsi;
-	struct regulator_bulk_data supplies[4];
+	struct regulator_bulk_data *supplies;
 	struct gpio_desc *reset_gpio;
-	const struct drm_display_mode *display_mode;
+	struct ch13726a_desc *desc;
 	enum drm_panel_orientation orientation;
-	bool prepared;
+};
+
+struct ch13726a_desc {
+	unsigned int width_mm;
+	unsigned int height_mm;
+	unsigned int bpc;
+
+	const struct drm_display_mode *modes;
+	unsigned int num_modes;
 };
 
 static inline struct ch13726a_panel *to_ch13726a_panel(struct drm_panel *panel)
@@ -34,11 +49,11 @@ static inline struct ch13726a_panel *to_ch13726a_panel(struct drm_panel *panel)
 
 static void ch13726a_reset(struct ch13726a_panel *ctx)
 {
-	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-	usleep_range(10000, 11000);
 	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
 	usleep_range(10000, 11000);
 	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
+	usleep_range(10000, 11000);
+	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
 	usleep_range(10000, 11000);
 }
 
@@ -78,10 +93,7 @@ static int ch13726a_prepare(struct drm_panel *panel)
 	struct device *dev = &ctx->dsi->dev;
 	int ret;
 
-	if (ctx->prepared)
-		return 0;
-
-	ret = regulator_bulk_enable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
+	ret = regulator_bulk_enable(ARRAY_SIZE(ch13726a_supplies), ctx->supplies);
 	if (ret < 0) {
 		dev_err(dev, "Failed to enable regulators: %d\n", ret);
 		return ret;
@@ -92,14 +104,12 @@ static int ch13726a_prepare(struct drm_panel *panel)
 	ret = ch13726a_on(ctx);
 	if (ret < 0) {
 		dev_err(dev, "Failed to initialize panel: %d\n", ret);
-		gpiod_set_value_cansleep(ctx->reset_gpio, 0);
-		regulator_bulk_disable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
+		gpiod_set_value_cansleep(ctx->reset_gpio, 1);
+		regulator_bulk_disable(ARRAY_SIZE(ch13726a_supplies), ctx->supplies);
 		return ret;
 	}
 
 	msleep(28);
-
-	ctx->prepared = true;
 
 	return 0;
 }
@@ -108,48 +118,76 @@ static int ch13726a_unprepare(struct drm_panel *panel)
 {
 	struct ch13726a_panel *ctx = to_ch13726a_panel(panel);
 
-	if (!ctx->prepared)
-		return 0;
+	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
+	regulator_bulk_disable(ARRAY_SIZE(ch13726a_supplies), ctx->supplies);
 
-	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
-	regulator_bulk_disable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
-
-	ctx->prepared = false;
 	return 0;
 }
 
-static const struct drm_display_mode thor_bottom_display_mode = {
-	.clock = (1080 + 28 + 4 + 36) * (1240 + 16 + 4 + 8) * 60 / 1000,
-	.hdisplay = 1080,
-	.hsync_start = 1080 + 28,
-	.hsync_end = 1080 + 28 + 4,
-	.htotal = 1080 + 28 + 4 + 36,
-	.vdisplay = 1240,
-	.vsync_start = 1240 + 16,
-	.vsync_end = 1240 + 16 + 4,
-	.vtotal = 1240 + 16 + 4 + 8,
+static const struct drm_display_mode thor_bottom_modes[] = {
+	{
+		/* 120Hz */
+		.clock = (1080 + 28 + 4 + 36) * (1240 + 16 + 4 + 8) * 120 / 1000,
+		.hdisplay = 1080,
+		.hsync_start = 1080 + 28,
+		.hsync_end = 1080 + 28 + 4,
+		.htotal = 1080 + 28 + 4 + 36,
+		.vdisplay = 1240,
+		.vsync_start = 1240 + 16,
+		.vsync_end = 1240 + 16 + 4,
+		.vtotal = 1240 + 16 + 4 + 8,
+	},
+	{
+		/* 60Hz */
+		.clock = (1080 + 28 + 4 + 36) * (1240 + 16 + 4 + 8) * 60 / 1000,
+		.hdisplay = 1080,
+		.hsync_start = 1080 + 28,
+		.hsync_end = 1080 + 28 + 4,
+		.htotal = 1080 + 28 + 4 + 36,
+		.vdisplay = 1240,
+		.vsync_start = 1240 + 16,
+		.vsync_end = 1240 + 16 + 4,
+		.vtotal = 1240 + 16 + 4 + 8,
+	}
+};
+
+static struct ch13726a_desc thor_bottom_desc = {
+	.modes = thor_bottom_modes,
+	.num_modes = ARRAY_SIZE(thor_bottom_modes),
 	.width_mm = 65,
 	.height_mm = 75,
+	.bpc = 8,
 };
 
 static int ch13726a_get_modes(struct drm_panel *panel,
 					struct drm_connector *connector)
 {
 	struct ch13726a_panel *ctx = to_ch13726a_panel(panel);
-	struct drm_display_mode *mode;
 
-	mode = drm_mode_duplicate(connector->dev, ctx->display_mode);
-	if (!mode)
-		return -ENOMEM;
+	for (uint8_t i = 0; i < ctx->desc->num_modes; i++) {
+		const struct drm_display_mode *m = &ctx->desc->modes[i];
+		struct drm_display_mode *mode;
 
-	drm_mode_set_name(mode);
+		mode = drm_mode_duplicate(connector->dev, m);
+		if (!mode) {
+			dev_err(&ctx->dsi->dev, "failed to add mode %ux%u@%u\n",
+				m->hdisplay, m->vdisplay, drm_mode_vrefresh(m));
+			return -ENOMEM;
+		}
 
-	mode->type = DRM_MODE_TYPE_DRIVER | DRM_MODE_TYPE_PREFERRED;
-	connector->display_info.width_mm = mode->width_mm;
-	connector->display_info.height_mm = mode->height_mm;
-	drm_mode_probed_add(connector, mode);
+		mode->type = DRM_MODE_TYPE_DRIVER;
+		if (i == 0)
+			mode->type |= DRM_MODE_TYPE_PREFERRED;
 
-	return 1;
+		drm_mode_set_name(mode);
+		drm_mode_probed_add(connector, mode);
+	}
+
+	connector->display_info.width_mm = ctx->desc->width_mm;
+	connector->display_info.height_mm = ctx->desc->height_mm;
+	connector->display_info.bpc = ctx->desc->bpc;
+
+	return ctx->desc->num_modes;
 }
 
 static enum drm_panel_orientation ch13726a_get_orientation(struct drm_panel *panel)
@@ -208,23 +246,24 @@ static int ch13726a_probe(struct mipi_dsi_device *dsi)
 	struct ch13726a_panel *ctx;
 	int ret;
 
-	ctx = devm_kzalloc(dev, sizeof(*ctx), GFP_KERNEL);
-	if (!ctx)
-		return -ENOMEM;
+	ctx = devm_drm_panel_alloc(dev, __typeof(*ctx), panel,
+				   &ch13726a_panel_funcs,
+				   DRM_MODE_CONNECTOR_DSI);
+	if (IS_ERR(ctx))
+		return PTR_ERR(ctx);
 
-	ctx->display_mode = of_device_get_match_data(dev);
+	ctx->desc = (struct ch13726a_desc *)of_device_get_match_data(dev);
+	if (!ctx->desc)
+		return -ENODEV;
 
-	ctx->supplies[0].supply = "vdd1v2";
-	ctx->supplies[1].supply = "vddio";
-	ctx->supplies[2].supply = "vdd";
-	ctx->supplies[3].supply = "avdd";
-
-	ret = devm_regulator_bulk_get(dev, ARRAY_SIZE(ctx->supplies),
-				      ctx->supplies);
+	ret = devm_regulator_bulk_get_const(dev,
+					    ARRAY_SIZE(ch13726a_supplies),
+					    ch13726a_supplies,
+					    &ctx->supplies);
 	if (ret < 0)
 		return dev_err_probe(dev, ret, "Failed to get regulators\n");
 
-	ctx->reset_gpio = devm_gpiod_get(dev, "reset", GPIOD_OUT_LOW);
+	ctx->reset_gpio = devm_gpiod_get(dev, "reset", GPIOD_OUT_HIGH);
 	if (IS_ERR(ctx->reset_gpio))
 		return dev_err_probe(dev, PTR_ERR(ctx->reset_gpio),
 				     "Failed to get reset-gpios\n");
@@ -243,8 +282,6 @@ static int ch13726a_probe(struct mipi_dsi_device *dsi)
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO |
 			  MIPI_DSI_CLOCK_NON_CONTINUOUS;
 
-	drm_panel_init(&ctx->panel, dev, &ch13726a_panel_funcs,
-		       DRM_MODE_CONNECTOR_DSI);
 	ctx->panel.prepare_prev_first = true;
 
 	ctx->panel.backlight = ch13726a_create_backlight(dsi);
@@ -277,7 +314,7 @@ static void ch13726a_remove(struct mipi_dsi_device *dsi)
 }
 
 static const struct of_device_id ch13726a_of_match[] = {
-	{ .compatible = "ayntec,thor-panel-bottom", .data = &thor_bottom_display_mode },
+	{ .compatible = "ayntec,thor-panel-bottom", .data = &thor_bottom_desc },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, ch13726a_of_match);
